2ca6b2bc23a4d9f66a70f0bddf9120932f7f320c
eng-kerolos-adel <eng.kerolos.adel.eleshaa@gmail.com>
2026-02-09 21:29:24 +0200

    done tow

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
+import { inngest } from "../../../inngest/client";
 import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/inngest/functions"
 
 // Create an API that serves zero functions
